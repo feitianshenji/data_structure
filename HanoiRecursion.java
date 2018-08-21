43b15ef4b24fb9a4014fc35ea4b21cf9c6a066f8
feitianshenji <40733073+feitianshenji@users.noreply.github.com>
2018-08-21 16:12:30 +0800

    汉诺塔的递归算法实现

---
+++ HanoiRecursion.java
@@ -0,0 +1,51 @@
+/**
+ * Created by zejian
+ * 汉诺塔的递归算法实现
+ */
+
+public class HanoiRecursion {
+
+    /**
+     * @param n 汉诺塔的层数
+     * @param x x柱 起点柱
+     * @param y y柱 目标柱
+     * @param z z柱 中转柱
+     */
+    public void hanoi(int n, char x, char y, char z) {
+
+        //H(0)=0
+        if (n == 0) {
+            //什么也不做
+        } else {
+            //H(n)=H(n-1)+1+H(n-1)
+            //将n-1个圆盘从x移动到z,y为中转柱
+            hanoi(n-1, x, z, y);//解出n-1层汉诺塔:H(n-1)
+
+            //移动最大圆盘到目的柱
+            System.out.println(x+"->"+y);
+
+            //将n-1个圆盘从z移动到y,x为中转柱
+            hanoi(n-1, z, y, x);//解出n-1层汉诺塔:H(n-1)
+        }
+    }
+
+    public int hanoiCount(int n, char x, char y, char z) {
+
+        int moveCount = 0;
+
+        if (n == 0) {
+            return 0;
+        } else {
+            moveCount = moveCount + hanoiCount(n-1, x, z, y);
+
+            moveCount = moveCount + 1;
+
+            moveCount = moveCount + hanoiCount(n-1, z, y, x);
+        }
+        return moveCount;
+    }
+    public static void main(String[] args) {
+        HanoiRecursion hanoi = new HanoiRecursion();
+        System.out.println("moveCount="+hanoi.hanoiCount(6,'A','B','C'));
+    }
+}
