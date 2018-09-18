738ad702e7506cffabe051644528e435bb1c6f18
feitianshenji <40733073+feitianshenji@users.noreply.github.com>
2018-09-19 06:45:28 +0800

    二叉树

--- Tree/BinaryTree/BinarySearchTree.java
+++ Tree/BinaryTree/BinarySearchTree.java
@@ -527,4 +527,91 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     public void clear() {
 
     }
+    /**
+     * 根据先根和中根遍历算法构造二叉树
+     * @param preList 先根遍历次序数组
+     * @param inList 中根遍历次序数组
+     * @param preStart
+     * @param preEnd
+     * @param inStart
+     * @param inEnd
+     * return root 最终返回的根节点
+     */
+    public BinaryNode<T> createBinarySearchTreeByPreIn(T[] preList, T[] inList, int preStart, int preEnd, int inStart, int inEnd) {
+
+        //创建根结点root
+        BinaryNode<T> p = new BinaryNode<>(preList[preStart]);
+        //如果没有其他元素，就说明结点已构建完成
+        if (preStart==preEnd && inStart==inEnd) {
+            return p;
+        }
+        //找出中根次序的根结点下标root
+        int root = 0;
+
+        for (root=inStart; root<inEnd; root++) {
+            //如果中根次序中的元素值与先根次序的根结点相当，则该下标index即为inList中的根结点下标
+            if (preList[preStart].compareTo(inList[root]) == 0) {
+                break;
+            }
+        }
+        //获取左子树的长度
+        int leftLength = root - inStart;
+        //获取右子树的长度
+        int rightLength = inEnd - root;
+
+        //递归构建左子树
+        if (leftLength > 0) {
+            //左子树的先根序列：preList[1],......,preList[i]
+            //左子树的中根序列：inList[0],......inList[i-1]
+            p.left = createBinarySearchTreeByPreIn(preList, inList, preStart+1, preStart+leftLength, inStart, root-1);
+        }
+        //构建右子树
+        if (rightLength > 0) {
+            //右子树的先根序列：preList[i+1],......,preList[n-1]
+            //右子树的中根序列：inList[i+1],......inList[n-1]
+            p.right = createBinarySearchTreeByPreIn(preList, inList, preStart+leftLength+1, preEnd, root+1, inEnd);
+        }
+        return p;
+    }
+    /**
+     * 后根/中根遍历构建二叉树
+     * @param postList 后根遍历序列
+     * @param inList 中根遍历序列
+     * @param postStart
+     * @param postEnd
+     * @param inStart
+     * @param inEnd
+     * @param 根结点
+     */
+    public BinaryNode<T> createBinarySearchTreeByPostIn(T[] postList, T[] inList, int postStart, int postEnd, int inStart, int inEnd) {
+
+        //构建根结点
+        BinaryNode<T> p = new BinaryNode<>(postList[postEnd]);
+        if (postStart==postEnd && inStart==inEnd) {
+            return p;
+        }
+        //查找中根序列的根结点下标root
+        int root = 0;
+        for (root=inStart; root<inEnd; root++) {
+            if (postList[postEnd].compareTo(inList[root])==0) {
+                break;
+            }
+        }
+
+        //左子树的长度
+        int leftLength = root - inStart;
+        //右子树的长度
+        int rightLength = inEnd - root;
+
+        //递归构建左子树
+        if (leftLength > 0) {
+            //postStart+leftLength-1;后根左子树的结束下标
+            p.left = createBinarySearchTreeByPostIn(postList, inList, postStart, postStart+leftLength-1, inStart, root-1);
+        }
+        //递归构建右子树
+        if (rightLength > 0) {
+            p.right = createBinarySearchTreeByPostIn(postList, inList, postStart+leftLength, postEnd-1, root+1, inEnd);
+        }
+        return p;
+    }
 }
